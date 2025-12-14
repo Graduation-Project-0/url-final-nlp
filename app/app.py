8ff7c0fd7fbf830a63a3ebc63f92ab6bf21c357b
Mohamed Reda <m07hamedreda25@gmail.com>
2025-12-15 01:12:16 +0200

    API Port Upgrade

--- app/app.py
+++ app/app.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=8002,
         reload=True,
         workers=4,
         log_level="info"
